f43ef99d1e5910016e70be2096798880abb945ed
Gaurang Telang <58006887+gstelang@users.noreply.github.com>
2024-02-19 17:14:46 -0800

    quick formatting

--- sorting-package.js
+++ sorting-package.js
@@ -9,29 +9,28 @@ const PACKAGE_TYPE = {
 
 const isPackageBulky = (width, height, length) => {
 
-    const isDimensionHigher =  [width, height, length].some(d => d >= MAX_DIMENSION);
+    const isDimensionHigher = [width, height, length].some(d => d >= MAX_DIMENSION);
 
     if (isDimensionHigher) {
-       return true;
+        return true;
     }
     // small optimization => dont multiply if this already exceeds the volume criteria for bulky.
     let v = width * height;
 
-    return v < MAX_VOLUME_BULKY 
-            ? v * length >= MAX_VOLUME_BULKY 
-            : true;
+    return v < MAX_VOLUME_BULKY
+        ? v * length >= MAX_VOLUME_BULKY
+        : true;
 }
 
 const isPackageHeavy = (mass) => {
-    return mass >= MAX_HEAVY
+    return mass >= MAX_HEAVY;
 }
-
 
 const sort = (width, height, length, mass) => {
 
     const isHeavy = isPackageHeavy(mass);
     const isBulky = isPackageBulky(width, height, length);
-  
+
     if (isHeavy && isBulky) {
         return PACKAGE_TYPE.REJECTED;
     }
@@ -43,13 +42,13 @@ const sort = (width, height, length, mass) => {
     return PACKAGE_TYPE.STANDARD;
 }
 
-const ans = sort(20, 20, 20, 40); // special because it is heavy
+const ans = sort(20, 20, 20, 40); // SPECIAL because it is heavy
 console.log(ans);
-const ans1 = sort(100, 1000, 100, 19); // special because it is bulky
+const ans1 = sort(100, 1000, 100, 19); // SPECIAL because it is bulky
 console.log(ans1);
 const ans2 = sort(100, 1000, 100, 20); // REJECTED
 console.log(ans2);
-const ans3 = sort(100, 1000, 9, 20); // rejected because it is bulk since dimension exceeds 150
+const ans3 = sort(100, 1000, 9, 20); // REJECTED because it is bulk since dimension exceeds 150
 console.log(ans3);
-const ans4 = sort(100, 100, 9, 19); // None
+const ans4 = sort(100, 100, 9, 19); // STANDARD
 console.log(ans4);
